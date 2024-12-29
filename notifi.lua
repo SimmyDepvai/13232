361215c4b3403409b43c34c437d6fba541ed1671
IamQhuynekk <157953172+SimmyDepvai@users.noreply.github.com>
2024-12-29 08:14:58 +0700

    Update notifi.lua

--- notifi.lua
+++ notifi.lua
@@ -3,7 +3,7 @@ local CoreGui = game:GetService("StarterGui")
 CoreGui:SetCore("SendNotification", {
 	Title = "Asolar";
 	Text = "Attached! | Made By @ngohuydepzai";
-		
+		Icon="rbxassetid://116667752016448";
 	Duration = 5;
 	
 })
